202b7f4d9eeb615df26a5697d1da12e6823f2ea2
Moshe Kravitz <moshe.kravitz@appricots.onmicrosoft.com>
2025-01-05 20:48:59 +0200

    login changes

--- login/login.js
+++ login/login.js
@@ -1,4 +1,34 @@
-function signUp() {
-    const bodyElem = document.getElementsByName("body");
-    console.log(bodyElem);
-}
+const loginButton = document.getElementById('login-button');
+const loginBox = document.getElementById('login-box');
+const SignInButton = document.getElementById('sign-in-button');
+const SignInBox = document.getElementById('sign-in-box');
+const overlay = document.getElementById('overlay');
+const closeLogin = document.getElementById('close-login');
+const closeSignIn = document.getElementById('close-sing-in');
+
+loginButton.addEventListener('click', () => {
+    loginBox.style.display = 'block';
+    overlay.style.display = 'block';
+});
+
+SignInButton.addEventListener('click', () => {
+    SignInBox.style.display = 'block';
+    overlay.style.display = 'block';
+});
+
+closeLogin.addEventListener('click', () => {
+    loginBox.style.display = 'none';
+    overlay.style.display = 'none';
+});
+
+closeSignIn.addEventListener('click', () => {
+    SignInBox.style.display = 'none';
+    overlay.style.display = 'none';
+});
+
+
+overlay.addEventListener('click', () => {
+    loginBox.style.display = 'none';
+    SignInBox.style.display = 'none';
+    overlay.style.display = 'none';
+});
