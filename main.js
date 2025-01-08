20485ada8f5cdd686490621e72b825d86a1547e3
Moshe Kravitz <moshe.kravitz@appricots.onmicrosoft.com>
2025-01-08 21:07:01 +0200

    css changes to login and minesweeper

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-console.log("main.js is loaded");
 
-let user = {info: null};
-
-
-
-//login = new Login(user,() => header.LoggedInUI());
-
-// Create a new instance of the Minesweeper game
-//const minesweeper = new MinesweeperUI('game-container', 'game-status', user);
-//minesweeper.initializeUI();
-console.log("asdf");
+let loginAttempts = localStorage.getItem("loginAttempts");
+if(!loginAttempts) {
+    localStorage.setItem("loginAttempts", 0);
+}
